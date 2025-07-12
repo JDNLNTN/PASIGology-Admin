af48b55e579d09027e3cfb79c94d61fb2278c6a7
JDNLNTN <alintonjohndaniel@gmail.com>
2025-07-12 10:05:44 +0800

    deplyment white screen fix 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Administrators from './pages/admin/Administrators';
 import Dialogue from './pages/dialogue/Dialogue';
